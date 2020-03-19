e84f0ebfc6eec0b3ce56bc7d841cd5629cf3afe9
alonecheer <alonecheer@hotmail.com>
2020-03-19 17:32:16 +0700

    add config

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyD9Y2mFgDg9nGo7MQjNvl2yZT32o5ymmvs",
+    authDomain: "client-lab8-firebase.firebaseapp.com",
+    databaseURL: "https://client-lab8-firebase.firebaseio.com",
+    projectId: "client-lab8-firebase",
+    storageBucket: "client-lab8-firebase.appspot.com",
+    messagingSenderId: "804170275254",
+    appId: "1:804170275254:web:6a7fb6ace9d97975f94917",
+    measurementId: "G-CL71QL3HCR"
+  };
+export default config;
